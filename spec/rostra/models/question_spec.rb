a02a9d43fb1d163f196b8a87d6fda785fbf731e4
Cory Schires <coryschires@gmail.com>
2011-09-18 14:31:54 -0500

    Added Rspec. Updated readme with info on how to run tests.

---
+++ spec/rostra/models/question_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+module Rostra
+  describe Question do
+
+    describe "VALIDATION beta code required" do
+      it "should be true" do
+        true.should be_true
+      end
+    end
+
+  end
+end
